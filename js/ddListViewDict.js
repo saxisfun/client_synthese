fe153bed9ea0ab2fa5df530a027cb0cab6ce12ff
ditedondaw <d.duplain@hotmail.com>
2014-02-22 16:47:34 -0500

    balise audio

--- js/ddListViewDict.js
+++ js/ddListViewDict.js
@@ -75,50 +75,44 @@ ClasseListViewDictionnaire.prototype.XMLHttpReqDict = function(le_url) {
  
 
  function obj2cell(le_id, l_espece,  la_descrip, le_IDPhoto) {
-	
-		
+			
  	var newDiv1 = document.createElement("div");
  	newDiv1.setAttribute("class", "la_liste1");
  	newDiv1.setAttribute("align", "left");
-	
  	newDiv1.id="id_div_liste_fichiers1";	
-	
- 	//newDiv1.addEventListener('click', test55, false)
- 	//newDiv1.addEventListener('click',function(){affichePageXXXXX(le_id, le_non_fich, le_url, la_descrip, la_date_eng)},false);
 
+	var audio = document.createElement('audio');
+	audio.src = 'sons/son001.mp3';
+	audio.controls = true;
+	audio.autoplay = false;
+	audio.setAttribute("class", "bird_audio");
+	newDiv1.appendChild(audio);
 
  	var newImg2 = document.createElement("img");
- 	newImg2.setAttribute("src", "img/musical_note.png");
+ 	newImg2.src="img/musical_note.png";
 	newImg2.setAttribute("class", "speaker_img");
-
+	newDiv1.appendChild(newImg2);
 
  	var newImg1 = document.createElement("img");
- 	newImg1.setAttribute("src", "birds/"+le_IDPhoto+".jpg");
+	newImg1.src="birds/"+le_IDPhoto+".jpg";
 	newImg1.setAttribute("class", "bird_img");
+    newDiv1.appendChild(newImg1);
 	
 	var newP2 = document.createElement("p");
  	var newP2Content = document.createTextNode(l_espece);
  	newP2.setAttribute("class", "bird_espece");
     newP2.appendChild(newP2Content);
+	newDiv1.appendChild(newP2);	
 	
  	var newP1 = document.createElement("p");
 	var newP1Content = document.createTextNode(la_descrip);
  	newP1.setAttribute("class", "bird_descript");
  	newP1.appendChild(newP1Content);	
-
-
-	newDiv1.appendChild(newImg2);
-	newDiv1.appendChild(newImg1);
-	newDiv1.appendChild(newP2);	
 	newDiv1.appendChild(newP1);	
-	
-	
-				
+							
  	my_mainDiv = document.getElementById("main");  
  	my_mainDiv.appendChild(newDiv1);
 	
-	
-		
  }
 
  
